b42f0f0536fd8b9308a651df9c7864a5922b1170
kratiDatacode <krati.datacode@gmail.com>
2025-07-22 18:43:03 +0530

    add working counter

--- react-redux-toolkit/src/App.jsx
+++ react-redux-toolkit/src/App.jsx
@@ -1,13 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment } from "./features/counter/counterSlice";
 
-import './App.css'
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
+  function handleIncrementClick() {
+    dispatch(increment());
+  }
+  function handleDecrementClick() {
+    dispatch(decrement());
+  }
   return (
     <>
-     hii
+      <div className="container">
+        <button onClick={handleIncrementClick}>+</button>
+        <p>Count:{count}</p>
+        <button onClick={handleDecrementClick}>-</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
